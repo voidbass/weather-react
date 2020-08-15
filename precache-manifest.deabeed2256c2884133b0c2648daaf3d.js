51538db6be02cd41249bd69f003e8cf5be79def7
Khôi Huỳnh <khoi.dn95@gmail.com>
2020-08-15 16:56:30 +0700

    Updates

---
+++ precache-manifest.deabeed2256c2884133b0c2648daaf3d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e2ea36c7011436bb834aae8e69eeb0",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "45b29a8d9bb0813b8625",
+    "url": "/weather-react/static/css/main.83011b58.chunk.css"
+  },
+  {
+    "revision": "44d91217a2106cc453b8",
+    "url": "/weather-react/static/js/2.6fc8ad99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-react/static/js/2.6fc8ad99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45b29a8d9bb0813b8625",
+    "url": "/weather-react/static/js/main.048b69f0.chunk.js"
+  },
+  {
+    "revision": "f0345a52712c5a71aa45",
+    "url": "/weather-react/static/js/runtime-main.523c1278.js"
+  },
+  {
+    "revision": "c0346ed70477a047d027ddb17ab66f6d",
+    "url": "/weather-react/static/media/bluesky-bg.c0346ed7.jpg"
+  },
+  {
+    "revision": "6e9d9230825910d252dfa73096abf65c",
+    "url": "/weather-react/static/media/cloud-bg.6e9d9230.jpg"
+  },
+  {
+    "revision": "8531c13da892ecbfb5f95c30efb2f774",
+    "url": "/weather-react/static/media/cold-bg.8531c13d.jpg"
+  },
+  {
+    "revision": "a550455cef61f385fb838e2f25b596d9",
+    "url": "/weather-react/static/media/cold-bg2.a550455c.png"
+  },
+  {
+    "revision": "7734792c92b65381a5749d25fa7dbfa5",
+    "url": "/weather-react/static/media/hot-bg.7734792c.png"
+  },
+  {
+    "revision": "93a89055156b7edb7c7bb73c0c3a7619",
+    "url": "/weather-react/static/media/rain-bg.93a89055.jpg"
+  },
+  {
+    "revision": "f110851ff1b011b0d6e18738e509f56d",
+    "url": "/weather-react/static/media/warm-bg.f110851f.jpg"
+  }
+]);
